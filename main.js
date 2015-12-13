b0b9f764ec032e61aa29948dbbf46d0f52033aaa
gembarrett <gem@gembarrett.com>
2015-12-12 20:12:33 -0500

    testing new data structure

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ var twitter;
 var services;
 
 d3.json("https://raw.githubusercontent.com/gembarrett/transparency-experiment/master/sample.json", function(data) {
-  console.log(data["apple"]);
+  console.log(data["apple"]["requests"]["early-2015"]);
 });
 
