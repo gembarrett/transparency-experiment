af0157aee0bdf110c471abd0bf4040a2ab68647d
gembarrett <gem@gembarrett.com>
2015-12-12 19:52:55 -0500

    messing around with importing data

---
+++ main.js
@@ -0,0 +1,9 @@
+var apple;
+var facebook;
+var twitter;
+var services;
+
+d3.json("https://raw.githubusercontent.com/gembarrett/transparency-experiment/master/sample.json", function(data) {
+  console.log(data["apple"]);
+});
+
