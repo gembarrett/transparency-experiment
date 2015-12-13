d335346bb3b24079b5b77c79b839abaeec70be31
gembarrett <gem@gembarrett.com>
2015-12-12 20:18:54 -0500

    new data structure updated with facebook and twitter info

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ var twitter;
 var services;
 
 d3.json("https://raw.githubusercontent.com/gembarrett/transparency-experiment/master/sample.json", function(data) {
-  console.log(data["apple"]["requests"]["early-2015"]);
+  console.log(data["apple"][0]["data"][0]["questions"][0]);
 });
 
