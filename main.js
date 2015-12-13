bac3760ba800297bf687f7a42b709e4e4b3bbe11
gembarrett <gem@gembarrett.com>
2015-12-12 20:25:50 -0500

    removing repeated bits

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ var twitter;
 var services;
 
 d3.json("https://raw.githubusercontent.com/gembarrett/transparency-experiment/master/sample.json", function(data) {
-  console.log(data["apple"][0]["data"][0]["questions"][0]);
+  console.log(data["apple"][0]["data"][0]["answers"][0]["numbers"][0]);
 });
 
