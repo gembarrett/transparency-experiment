d99d97fbc1ef90ad0ef847f0e28707176c77e63d
gembarrett <gem@gembarrett.com>
2015-12-13 00:49:14 -0500

    committing before breaking stuff by trying out a stacked chart

--- main.js
+++ main.js
@@ -29,9 +29,6 @@ function getQuestions() {
     twitterQ[i] = twitter["data"][0]["questions"][i];
     facebookQ[i] = facebook["data"][0]["questions"][i];
   }
-  appleQ.reverse();
-  twitterQ.reverse();
-  facebookQ.reverse();
 }
 
 function getAnswers() {
@@ -50,7 +47,8 @@ function getAnswers() {
 }
 
 function visualiseIt() {
-  // var tempFull = appleQ.concat(twitterQ).concat(facebookQ);
+  
+  var tempFull = appleQ.concat(twitterQ).concat(facebookQ);
   var fullWidth = window.innerWidth;
 
   var width = fullWidth - 100,
@@ -62,24 +60,43 @@ function visualiseIt() {
   var chart = d3.select(".chart")
               .attr("width", width);
 
-  x.domain([0, d3.max(appleQ,function(d) { return d })]);
+  x.domain([0, d3.max(tempFull,function(d) { return d })]);
 
   chart.attr("height", barHeight * appleQ.length);
 
-  var bar = chart.selectAll("g")
+  var appleBar = chart.selectAll("g")
             .data(appleQ)
             .enter()
             .append("g")
             .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
 
-  bar.append("rect")
+  appleBar.append("rect")
       .attr("width", function(d) { return x (d); })
       .attr("height", barHeight - 1)
       .style("fill", "lightblue");
 
-  bar.append("text")
+  appleBar.append("text")
       .attr("x", function(d) { return x(d) - 50; })
       .attr("y", barHeight/2)
       .attr("dy", ".35em")
       .text(function(d) { return d; });
+
+  var twitterBar = chart.selectAll("g")
+            .data(twitterQ)
+            .enter()
+            .append("g")
+            .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+  twitterBar.append("rect")
+      .attr("width", function(d) { return x(d); })
+      .attr("height", barHeight - 1)
+      .style("fill", "lightgreen");
+
+  twitterBar.append("text")
+      .attr("x", function(d) { return x(d) - 10; })
+      .attr("y", barHeight/2)
+      .attr("dy", ".35em")
+      .text(function(d) { return d; });
+
+
 }
