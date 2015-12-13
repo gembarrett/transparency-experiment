5c8b7ba2843ddf58118551110d3d821bf0d1ed04
gembarrett <gem@gembarrett.com>
2015-12-12 23:52:11 -0500

    got a bar graph thing going on now, I like it

--- main.js
+++ main.js
@@ -50,42 +50,36 @@ function getAnswers() {
 }
 
 function visualiseIt() {
-  var appleSvg = d3.select("body")
-            .append("svg")
-            .attr("height", 120)
-            .attr("width", 720);
+  // var tempFull = appleQ.concat(twitterQ).concat(facebookQ);
+  var fullWidth = window.innerWidth;
 
-  var appleCircles = appleSvg.selectAll("circle")
-                .data(appleQ)
-                .enter()
-                .append("circle")
-                .attr("cy", 60)
-                .attr("cx", function(d,i) { return i * 100 + 30; })
-                .attr("r", function(d) { return d/500; });
+  var width = fullWidth - 100,
+      barHeight = 20;
 
-  var twitterSvg = d3.select("body")
-            .append("svg")
-            .attr("height", 120)
-            .attr("width", 720);
+  var x = d3.scale.linear()
+          .range([0, width]);
 
-  var twitterCircles = twitterSvg.selectAll("circle")
-                .data(twitterQ)
-                .enter()
-                .append("circle")
-                .attr("cy", 60)
-                .attr("cx", function(d,i) { return i * 100 + 30; })
-                .attr("r", function(d) { return d/500; });
+  var chart = d3.select(".chart")
+              .attr("width", width);
 
-  var facebookSvg = d3.select("body")
-            .append("svg")
-            .attr("height", 120)
-            .attr("width", 720);
+  x.domain([0, d3.max(appleQ,function(d) { return d })]);
 
-  var facebookCircles = facebookSvg.selectAll("circle")
-                .data(facebookQ)
-                .enter()
-                .append("circle")
-                .attr("cy", 60)
-                .attr("cx", function(d,i) { return i * 100 + 30; })
-                .attr("r", function(d) { return d/500; });
+  chart.attr("height", barHeight * appleQ.length);
+
+  var bar = chart.selectAll("g")
+            .data(appleQ)
+            .enter()
+            .append("g")
+            .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+  bar.append("rect")
+      .attr("width", function(d) { return x (d); })
+      .attr("height", barHeight - 1)
+      .style("fill", "lightblue");
+
+  bar.append("text")
+      .attr("x", function(d) { return x(d) - 50; })
+      .attr("y", barHeight/2)
+      .attr("dy", ".35em")
+      .text(function(d) { return d; });
 }
